a40f4339e1ca2dbe747785540bb94f505500b882
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-06-27 14:23:13 +0100

    Task 02

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {
-      console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
     })
-    .catch((error) => error);
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
