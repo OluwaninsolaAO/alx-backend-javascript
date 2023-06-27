c8a4cdec6712c49afd8d23badd65989c1b2e591b
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-06-27 17:59:47 +0100

    Task 06

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,12 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  return Promise.all([user, photo])
-    .then(([vUser, vPhoto]) => [
-      { status: 'success', value: vUser },
-      { status: 'success', value: vPhoto },
-    ])
-    .catch(() => {});
+  return Promise.allSettled([user, photo]).then((resp) => [
+    resp.map((res) => {
+      if (res.status === 'fulfilled') {
+        return { status: res.status, value: res.value };
+      }
+      return { status: res.status, value: res.reason };
+    }),
+  ]);
 }
