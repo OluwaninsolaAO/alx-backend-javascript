f7d06b5850f6246fb733e1e3dde1dc337fa706f1
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-06-27 13:20:32 +0100

    Create helper script for project setup.

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Sets up Project's dependencies
+
+npm install --save-dev jest
+npm install --save-dev babel-jest @babel/core @babel/preset-env @babel/cli
+npm install --save-dev eslint
