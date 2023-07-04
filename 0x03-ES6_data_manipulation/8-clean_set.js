53529e9e995c3b43a30d9410b96a04723dd01695
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-04 08:41:57 +0100

    Task 08

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  const array = new Array();
+  const array = [];
   if (startString === '') {
     return '';
   }
   for (const member of set) {
-    member.startsWith(startString)
-      ? array.push(member.replace(startString, ''))
-      : '';
+    if (member.startsWith(startString)) {
+      array.push(member.replace(startString, ''));
+    }
   }
   return array.join('-');
 }
