2a8b6b066c6d9ab71d58f71a3a1426f5568376de
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-08-21 20:54:40 +0100

    Task 01

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 // 1. Using Process stdin
 
-process.stdin.setEncoding('utf-8');
-process.stdout.write('Welcome to Holberton School,what is your name?\n');
+const { stdout, stdin } = process;
 
-process.stdin.on('readable', () => {
-  process.stdout.write(`Your name is: ${process.stdin.read()}`);
+stdout.write('Welcome to Holberton School, what is your name?\n');
+
+stdin.setEncoding('utf-8');
+stdin.on('readable', () => {
+  stdout.write(`Your name is: ${stdin.read()}`);
   process.exit();
 });
 
 process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
+  stdout.write('This important software is now closing\n');
 });
