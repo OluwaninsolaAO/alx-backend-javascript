f2de535bfb9bc6b1f481a5527593be375826fc22
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-08-22 12:39:43 +0100

    Task 02

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+// 2. Reading a file synchronously with Node JS
+const { readFileSync } = require('fs');
+
+const delimeter = '\r\n';
+
+function countStudents(path) {
+  try {
+    const data = readFileSync(path, 'utf-8');
+    const rows = data.split(delimeter);
+    const students = [];
+    const fields = new Set();
+    const header = rows.shift().split(',');
+    for (let row of rows) {
+      row = row.split(',');
+      const student = Object.fromEntries(
+        header.map((key, index) => [key, row[index]]),
+      );
+      students.push(student);
+      fields.add(student.field);
+    }
+
+    console.log(`Number of students: ${students.length}`);
+    fields.forEach((field) => {
+      const studentsInField = students.filter(
+        (student) => student.field === field,
+      );
+      console.log(
+        `Number of students in ${field}: ${
+          studentsInField.length
+        }. List: ${studentsInField
+          .map((student) => student.firstname)
+          .join(', ')}`,
+      );
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
